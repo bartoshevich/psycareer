358145ed053143753808d85b8a4498fe904e994f
Bartoshevich <25452644+bartoshevich@users.noreply.github.com>
2020-12-27 09:54:50 +0300

    2

--- service-workers.js
+++ service-workers.js
@@ -2,19 +2,27 @@
 layout: null
 ---
 
-var version = '{{site.time | date: '%Y%m%d%H%M%S'}}::';
+'use strict'
+
+let version = '{{site.time | date: '%Y%m%d%H%M%S'}}::';
 
 
-var urlsToCache = [
+let urlsToCache = [
   '/',
-  '/poleznye-ssylki/',
+  '/me/',
   '/author/',
-  '/style/style.css'
+  '/poleznye-ssylki/',
+  '/style/style.css',
+  '/uzkaya-specializaciya/',
+  '/tabu-v-reklame/',
+  '/sfery-specializacii-psixologa/',
+  '/sovet-nachinayushhim-psixologam-po-reklame-uslug/',
+  '/rekomendacii-po-napolneniyu-sajta-dlya-psixologa/',
+  '/sait-psihologa-tipichnie-oshibki/'
  ];
 
 
-
-var CACHE_NAME = 'version + psycareer';
+let CACHE_NAME = 'version + psycareer';
 
 self.addEventListener('install', function(evt) {
   evt.waitUntil(
@@ -27,8 +35,9 @@ self.addEventListener('install', function(evt) {
 });
 
 
+
 self.addEventListener('activate', (event) => {
-  var cacheKeeplist = ['CACHE_NAME'];
+  let cacheKeeplist = ['CACHE_NAME'];
 
   event.waitUntil(
     caches.keys().then((keyList) => {
@@ -64,78 +73,3 @@ self.addEventListener('fetch', function(event) {
     }
   }));
 });
-
-{% comment %}
-
-
-
-var CACHE = 'cache-update-and-refresh';
-
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-
-  evt.waitUntil(caches.open(CACHE).then(function (cache) {
-    cache.addAll([
-      '/',
-      '/uzkaya-specializaciya/',
-      '/poleznye-ssylki/',
-      '/author/',
-      '/style/likely.css',
-      '/tabu-v-reklame/',
-      '/sfery-specializacii-psixologa/',
-      '/style/style.css'
-    ]);
-  }));
-});
-
-
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.');
-
-  evt.respondWith(fromCache(evt.request));
-
-  evt.waitUntil(
-    update(evt.request)
-
-    .then(refresh)
-  );
-});
-
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request);
-  });
-}
-
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
-    });
-  });
-}
-
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-
-      var message = {
-        type: 'refresh',
-        url: response.url,
-
-        eTag: response.headers.get('ETag')
-     };
-
-     client.postMessage(JSON.stringify(message));
-    });
-  });
-}
-
-
-
-
-
-
-{% endcomment %}
